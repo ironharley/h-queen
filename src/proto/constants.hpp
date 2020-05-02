b094e5d959bac8be3be27f0677f685477202f4cb
Harley Cooper <5398020@gmail.com>
2020-05-02 02:50:58 +0000

    constants + renamed

---
+++ src/proto/constants.hpp
@@ -0,0 +1,27 @@
+/*
+ * constants.hpp
+ *
+ *  Created on: May 2, 2020
+ *      Author: hkooper
+ */
+
+#ifndef SRC_PROTO_CONSTANTS_HPP_
+#define SRC_PROTO_CONSTANTS_HPP_
+#include <sys/types.h>
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+namespace hqn {
+namespace config {
+class defaults {
+public:
+	static const uint16_t server_port = 34434;
+};
+}}
+
+
+
+#endif /* SRC_PROTO_CONSTANTS_HPP_ */
